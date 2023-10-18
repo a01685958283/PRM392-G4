6bd9479a7e744d2b0fd86f3342658e72e7d968a3
Đoàn Tiến Đạt <doandat131102@gmail.com>
2023-10-18 11:59:32 +0700

    New Package
    Domain and Adapter

---
+++ app/src/main/java/com/example/Adapter/EmptyAdapter.java
@@ -0,0 +1,8 @@
+package com.example.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EmptyAdapter {
+}
--- app/src/main/java/com/example/teamprm392/Active/MainActivity.java
+++ app/src/main/java/com/example/teamprm392/Active/MainActivity.java
@@ -1,16 +1,39 @@
 package com.example.teamprm392.Active;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.teamprm392.R;
+import com.example.temprm392.Domain.CategoryDomain;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+private RecyclerView.Adapter adapter,adapter2;
+private RecyclerView recyclerViewCategoryList,recyclerViewPopularlist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewCategory();
+    }
+
+    private void recyclerViewCategory(){
+        LinearLayoutManager linerLayoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewCategoryList=findViewById(R.id.btn_view1);
+        recyclerViewCategoryList.setLayoutManager(linerLayoutManager);
+
+        ArrayList<CategoryDomain> categoryList=new ArrayList<>();
+        categoryList.add(new CategoryDomain("Pizza","cat_1"));
+        categoryList.add(new CategoryDomain("Burger","cat_2"));
+        categoryList.add(new CategoryDomain("Hotdog","cat_3"));
+        categoryList.add(new CategoryDomain("Drink","cat_4"));
+        categoryList.add(new CategoryDomain("Donut","cat_5"));
+
+        adapter=
     }
 }
